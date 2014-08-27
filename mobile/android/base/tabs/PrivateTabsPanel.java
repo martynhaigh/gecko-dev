cb821f6a366200dc7871999751a72d4de710933c
Martyn Haigh <martyn.haigh@gmail.com>
2014-08-27 16:14:12 +0100

    rename var

--- mobile/android/base/tabs/PrivateTabsPanel.java
+++ mobile/android/base/tabs/PrivateTabsPanel.java
@@ -27,16 +27,16 @@ import android.widget.FrameLayout;
  */
 class PrivateTabsPanel extends FrameLayout implements CloseAllPanelView {
     private TabsPanel tabsPanel;
-    private TabsLayout tabsTray;
+    private TabsLayout tabsLayout;
 
     public PrivateTabsPanel(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         LayoutInflater.from(context).inflate(R.layout.private_tabs_panel, this);
-        tabsTray = (TabsLayout) findViewById(R.id.private_tabs_tray);
+        tabsLayout = (TabsLayout) findViewById(R.id.private_tabs_tray);
 
         final View emptyView = findViewById(R.id.private_tabs_empty);
-        tabsTray.setEmptyView(emptyView);
+        tabsLayout.setEmptyView(emptyView);
 
         final View learnMore = findViewById(R.id.private_tabs_learn_more);
         learnMore.setOnClickListener(new OnClickListener() {
@@ -56,28 +56,28 @@ class PrivateTabsPanel extends FrameLayout implements CloseAllPanelView {
     @Override
     public void setTabsPanel(TabsPanel panel) {
         tabsPanel = panel;
-        tabsTray.setTabsPanel(panel);
+        tabsLayout.setTabsPanel(panel);
     }
 
     @Override
     public void show() {
-        tabsTray.show();
+        tabsLayout.show();
         setVisibility(View.VISIBLE);
     }
 
     @Override
     public void hide() {
         setVisibility(View.GONE);
-        tabsTray.hide();
+        tabsLayout.hide();
     }
 
     @Override
     public boolean shouldExpand() {
-        return tabsTray.shouldExpand();
+        return tabsLayout.shouldExpand();
     }
 
     @Override
     public void closeAll() {
-        tabsTray.closeAll();
+        tabsLayout.closeAll();
     }
 }
