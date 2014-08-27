a2f08a0516296b625a2d34ad462160955f6b3051
Martyn Haigh <martyn.haigh@gmail.com>
2014-08-27 10:32:27 +0100

    wip

--- mobile/android/base/tabs/TabsTrayList.java
+++ mobile/android/base/tabs/TabsTrayList.java
@@ -14,7 +14,7 @@ import org.mozilla.gecko.GeckoEvent;
 import org.mozilla.gecko.R;
 import org.mozilla.gecko.Tab;
 import org.mozilla.gecko.Tabs;
-import org.mozilla.gecko.tabs.TabsTray;
+import org.mozilla.gecko.tabs.TabsPanel.TabsTray;
 import org.mozilla.gecko.animation.PropertyAnimator;
 import org.mozilla.gecko.animation.PropertyAnimator.Property;
 import org.mozilla.gecko.animation.ViewHelper;
@@ -40,8 +40,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 class TabsTrayList extends TwoWayView
-                   implements TabsTray,
-                              TabsPanel.CloseAllPanelView {
+                   implements TabsTray {
     private static final String LOGTAG = "Gecko" + TabsTrayList.class.getSimpleName();
 
     private Context mContext;
