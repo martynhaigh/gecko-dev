16bda65681b96e9ddc09ba00a37913556362286a
Martyn Haigh <martyn.haigh@gmail.com>
2014-09-03 11:51:12 +0100

    remove unused imports

--- mobile/android/base/tabs/TabsLayoutItemView.java
+++ mobile/android/base/tabs/TabsLayoutItemView.java
@@ -12,7 +12,6 @@ import org.mozilla.gecko.widget.TabThumbnailWrapper;
 import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
