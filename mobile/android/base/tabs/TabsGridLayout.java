0ddeaf9479f7382c9c47dcb7d0ac3a9525989579
Martyn Haigh <martyn.haigh@gmail.com>
2014-09-18 12:29:28 -0700

    fix nits

--- mobile/android/base/tabs/TabsGridLayout.java
+++ mobile/android/base/tabs/TabsGridLayout.java
@@ -8,23 +8,18 @@ package org.mozilla.gecko.tabs;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.mozilla.gecko.animation.PropertyAnimator.Property;
-import org.mozilla.gecko.animation.PropertyAnimator;
 import org.mozilla.gecko.animation.ViewHelper;
 import org.mozilla.gecko.GeckoAppShell;
 import org.mozilla.gecko.GeckoEvent;
 import org.mozilla.gecko.R;
 import org.mozilla.gecko.Tab;
-import org.mozilla.gecko.tabs.TabsLayoutAdapter;
 import org.mozilla.gecko.tabs.TabsPanel.TabsLayout;
 import org.mozilla.gecko.Tabs;
-import org.mozilla.gecko.util.ThreadUtils;
 
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.GridView;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -47,22 +42,12 @@ class TabsGridLayout extends GridView
     private TabsLayoutAdapter mTabsAdapter;
 
     private List<View> mPendingClosedTabs;
-    private int mCloseAnimationCount;
-    private int mCloseAllAnimationCount;
-
-    // Time to animate non-flinged tabs of screen, in milliseconds
-    private static final int ANIMATION_DURATION = 250;
-
-    // Time between starting successive tab animations in closeAllTabs.
-    private static final int ANIMATION_CASCADE_DELAY = 75;
-
-    private int mOriginalSize;
 
     public TabsGridLayout(Context context, AttributeSet attrs) {
         super(context, attrs, R.attr.tabGridLayoutViewStyle);
         mContext = context;
 
-        mPendingClosedTabs = new ArrayList<View>();
+        mPendingClosedTabs = new ArrayList<>();
 
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.TabsTray);
         mIsPrivate = (a.getInt(R.styleable.TabsTray_tabs, 0x0) == 1);
@@ -82,16 +67,29 @@ class TabsGridLayout extends GridView
     }
 
     private class TabsGridLayoutAdapter extends TabsLayoutAdapter {
-        private Button.OnClickListener mOnClickListener;
+
+        private Button.OnClickListener mCloseClickListener;
+        private View.OnClickListener mSelectClickListener;
+
         public TabsGridLayoutAdapter (Context context) {
             super(context);
 
-            mOnClickListener = new Button.OnClickListener() {
+            mCloseClickListener = new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    TabsLayoutItemView itemView = (TabsLayoutItemView) v.getTag();
+                    Tab tab = Tabs.getInstance().getTab(itemView.id);
+                    Tabs.getInstance().closeTab(tab);
+                }
+            };
+
+            mSelectClickListener = new View.OnClickListener() {
+
                 @Override
                 public void onClick(View v) {
                     TabsLayoutItemView tab = (TabsLayoutItemView) v.getTag();
-                    final int pos = (isVertical() ? tab.info.getWidth() : 0 - tab.info.getHeight());
-                    animateClose(tab.info, pos);
+                    Tabs.getInstance().selectTab(tab.id);
+                    TabsGridLayout.this.autoHidePanel();
                 }
             };
         }
@@ -103,7 +101,7 @@ class TabsGridLayout extends GridView
             // This is nasty and once we change TabsLayoutItemView to an actual view
             // we can get rid of it.
             TabsLayoutItemView item = (TabsLayoutItemView) view.getTag();
-            item.close.setOnClickListener(mOnClickListener);
+            item.close.setOnClickListener(mCloseClickListener);
 
             return view;
         }
@@ -111,21 +109,13 @@ class TabsGridLayout extends GridView
         @Override
         public void bindView(View view, Tab tab) {
             super.bindView(view, tab);
-            view.setOnClickListener(new View.OnClickListener() {
 
-                @Override
-                public void onClick(View v) {
-                    TabsLayoutItemView tab = (TabsLayoutItemView) v.getTag();
-                    Tabs.getInstance().selectTab(tab.id);
-                    TabsGridLayout.this.autoHidePanel();
-                }
-            });
+            view.setOnClickListener(mSelectClickListener);
 
             // If we're recycling this view, there's a chance it was transformed during
             // the close animation. Remove any of those properties.
             TabsGridLayout.this.resetTransforms(view);
         }
-
     }
 
     @Override
@@ -151,7 +141,7 @@ class TabsGridLayout extends GridView
 
     @Override
     public boolean shouldExpand() {
-        return isVertical();
+        return true;
     }
 
     private void autoHidePanel() {
@@ -231,172 +221,40 @@ class TabsGridLayout extends GridView
 
     public void resetTransforms(View view) {
         ViewHelper.setAlpha(view, 1);
+        ViewHelper.setTranslationX(view, 0);
 
-        if (isVertical()) {
-            ViewHelper.setTranslationX(view, 0);
-        } else {
-            ViewHelper.setTranslationY(view, 0);
-        }
-
-        // We only need to reset the height or width after individual tab close animations.
-        if (mOriginalSize != 0) {
-            ViewHelper.setHeight(view, mOriginalSize);
-        }
+//
+//        // We only need to reset the height or width after individual tab close animations.
+//        if (mOriginalSize != 0) {
+//            ViewHelper.setHeight(view, mOriginalSize);
+//        }
     }
 
-    private boolean isVertical() {
-        return true;
-    }
+
 
     @Override
     public void closeAll() {
-        final int childCount = getChildCount();
 
         // Just close the panel if there are no tabs to close.
-        if (childCount == 0) {
+        if (getChildCount() == 0) {
             autoHidePanel();
             return;
         }
 
-        // Disable the view so that gestures won't interfere wth the tab close animation.
-        setEnabled(false);
+        // Hide the panel after the animation is done.
+        autoHidePanel();
 
-        // Delay starting each successive animation to create a cascade effect.
-        int cascadeDelay = 0;
+        // Re-enable the view after the animation is done.
+        TabsGridLayout.this.setEnabled(true);
 
-        for (int i = childCount - 1; i >= 0; i--) {
-            final View view = getChildAt(i);
-            final PropertyAnimator animator = new PropertyAnimator(ANIMATION_DURATION);
-            animator.attach(view, Property.ALPHA, 0);
-
-            animator.attach(view, Property.TRANSLATION_X, view.getWidth());
-
-            mCloseAllAnimationCount++;
-
-            animator.addPropertyAnimationListener(new PropertyAnimator.PropertyAnimationListener() {
-                @Override
-                public void onPropertyAnimationStart() { }
-
-                @Override
-                public void onPropertyAnimationEnd() {
-                    mCloseAllAnimationCount--;
-                    if (mCloseAllAnimationCount > 0) {
-                        return;
-                    }
-
-                    // Hide the panel after the animation is done.
-                    autoHidePanel();
-
-                    // Re-enable the view after the animation is done.
-                    TabsGridLayout.this.setEnabled(true);
-
-                    // Then actually close all the tabs.
-                    final Iterable<Tab> tabs = Tabs.getInstance().getTabsInOrder();
-                    for (Tab tab : tabs) {
-                        // In the normal panel we want to close all tabs (both private and normal),
-                        // but in the private panel we only want to close private tabs.
-                        if (!mIsPrivate || tab.isPrivate()) {
-                            Tabs.getInstance().closeTab(tab, false);
-                        }
-                    }
-                }
-            });
-
-            ThreadUtils.getUiHandler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    animator.start();
-                }
-            }, cascadeDelay);
-
-            cascadeDelay += ANIMATION_CASCADE_DELAY;
+        // Then actually close all the tabs.
+        final Iterable<Tab> tabs = Tabs.getInstance().getTabsInOrder();
+        for (Tab tab : tabs) {
+            // In the normal panel we want to close all tabs (both private and normal),
+            // but in the private panel we only want to close private tabs.
+            if (!mIsPrivate || tab.isPrivate()) {
+                Tabs.getInstance().closeTab(tab, false);
+            }
         }
     }
-
-    private void animateClose(final View view, int pos) {
-        PropertyAnimator animator = new PropertyAnimator(ANIMATION_DURATION);
-        animator.attach(view, Property.ALPHA, 0);
-
-        animator.attach(view, Property.TRANSLATION_X, pos);
-
-        mCloseAnimationCount++;
-        mPendingClosedTabs.add(view);
-
-        animator.addPropertyAnimationListener(new PropertyAnimator.PropertyAnimationListener() {
-            @Override
-            public void onPropertyAnimationStart() { }
-            @Override
-            public void onPropertyAnimationEnd() {
-                mCloseAnimationCount--;
-                if (mCloseAnimationCount > 0)
-                    return;
-
-                for (View pendingView : mPendingClosedTabs) {
-                    animateFinishClose(pendingView);
-                }
-
-                mPendingClosedTabs.clear();
-            }
-        });
-
-        if (mTabsAdapter.getCount() == 1)
-            autoHidePanel();
-
-        animator.start();
-    }
-
-    private void animateFinishClose(final View view) {
-        PropertyAnimator animator = new PropertyAnimator(ANIMATION_DURATION);
-
-        final boolean isVertical = isVertical();
-        if (isVertical)
-            animator.attach(view, Property.HEIGHT, 1);
-        else
-            animator.attach(view, Property.WIDTH, 1);
-
-        TabsLayoutItemView tab = (TabsLayoutItemView)view.getTag();
-        final int tabId = tab.id;
-
-        // Caching this assumes that all rows are the same height
-        if (mOriginalSize == 0) {
-            mOriginalSize = view.getHeight();
-        }
-
-        animator.addPropertyAnimationListener(new PropertyAnimator.PropertyAnimationListener() {
-            @Override
-            public void onPropertyAnimationStart() { }
-            @Override
-            public void onPropertyAnimationEnd() {
-                Tabs tabs = Tabs.getInstance();
-                Tab tab = tabs.getTab(tabId);
-                tabs.closeTab(tab, true);
-            }
-        });
-
-        animator.start();
-    }
-
-    private void animateCancel(final View view) {
-        PropertyAnimator animator = new PropertyAnimator(ANIMATION_DURATION);
-        animator.attach(view, Property.ALPHA, 1);
-
-        if (isVertical())
-            animator.attach(view, Property.TRANSLATION_X, 0);
-        else
-            animator.attach(view, Property.TRANSLATION_Y, 0);
-
-
-        animator.addPropertyAnimationListener(new PropertyAnimator.PropertyAnimationListener() {
-            @Override
-            public void onPropertyAnimationStart() { }
-            @Override
-            public void onPropertyAnimationEnd() {
-                TabsLayoutItemView tab = (TabsLayoutItemView) view.getTag();
-                tab.close.setVisibility(View.VISIBLE);
-            }
-        });
-
-        animator.start();
-    }
-
 }
