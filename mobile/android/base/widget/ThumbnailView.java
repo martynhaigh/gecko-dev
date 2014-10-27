ce9d6152c4053c70e5e36d03a50aab1ea1224afe
Martyn Haigh <martyn.haigh@gmail.com>
2014-10-27 12:59:51 +0000

    revert unnecessary change

--- mobile/android/base/widget/ThumbnailView.java
+++ mobile/android/base/widget/ThumbnailView.java
@@ -36,7 +36,7 @@ public class ThumbnailView extends ImageView {
             int h1 = d.getIntrinsicHeight();
             int w2 = getWidth();
             int h2 = getHeight();
-
+    
             float scale = (w2/h2 < w1/h1) ? (float)h2/h1 : (float)w2/w1;
             mMatrix.setScale(scale, scale);
         }
