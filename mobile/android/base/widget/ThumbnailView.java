ca92c1086a2a5a87e8219f0f4f1ce7b6651635d5
Martyn Haigh <martyn.haigh@gmail.com>
2014-10-30 17:57:23 +0000

    omg it works

--- mobile/android/base/widget/ThumbnailView.java
+++ mobile/android/base/widget/ThumbnailView.java
@@ -5,30 +5,74 @@
 
 package org.mozilla.gecko.widget;
 
-import org.mozilla.gecko.R;
-
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.widget.ImageView;
 
+/* Special version of ImageView for thumbnails. Scales a thumbnail so that it maintains its aspect
+ * ratio and so that the images width and height are the same size or greater than the view size
+ */
 public class ThumbnailView extends ImageView {
     private static final String LOGTAG = "GeckoThumbnailView";
+    final private Matrix mMatrix;
+    private int mWidthSpec = -1;
+    private int mHeightSpec = -1;
+    private boolean mLayoutChanged;
+    private boolean mScale = false;
 
     public ThumbnailView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mMatrix = new Matrix();
+        mLayoutChanged = true;
     }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        Drawable d = getDrawable();
+        if (mLayoutChanged && mScale) {
+            int w1 = d.getIntrinsicWidth();
+            int h1 = d.getIntrinsicHeight();
+            int w2 = getWidth();
+            int h2 = getHeight();
+
+            float scale = (w2/h2 < w1/h1) ? (float)h2/h1 : (float)w2/w1;
+            mMatrix.setScale(scale, scale);
+        }
+
+        int saveCount = canvas.save();
+        canvas.concat(mMatrix);
+        d.draw(canvas);
+        canvas.restoreToCount(saveCount);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // OnLayout.changed isn't a reliable measure of whether or not the size of this view has changed
+        // neither is onSizeChanged called often enough. Instead, we track changes in size ourselves, and
+        // only invalidate this matrix if we have a new width/height spec
+        if (widthMeasureSpec != mWidthSpec || heightMeasureSpec != mHeightSpec) {
+            mWidthSpec = widthMeasureSpec;
+            mHeightSpec = heightMeasureSpec;
+            mLayoutChanged = true;
+        }
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
 
     @Override
     public void setImageDrawable(Drawable drawable) {
         if (drawable == null) {
-            drawable = getResources().getDrawable(R.drawable.new_tablet_tab_background);
-            Resources r = getResources();
-            float heightPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 140, r.getDisplayMetrics());
-            float widthPx = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 140, r.getDisplayMetrics());
-            drawable.setBounds(0, 0, (int)widthPx, (int)heightPx);
+            drawable = getResources().getDrawable(org.mozilla.gecko.R.drawable.new_tablet_tab_background);
+            setScaleType(ScaleType.FIT_XY);
+            mScale = false;
+        } else {
+            mScale = true;
+            setScaleType(ScaleType.FIT_CENTER);
         }
         super.setImageDrawable(drawable);
     }
