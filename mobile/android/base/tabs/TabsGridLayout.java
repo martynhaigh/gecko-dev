2e685f9530745c645ab93eac5b2af8a14d814875
Martyn Haigh <martyn.haigh@gmail.com>
2014-11-25 15:09:25 +0000

    optimise imports

--- mobile/android/base/tabs/TabsGridLayout.java
+++ mobile/android/base/tabs/TabsGridLayout.java
@@ -18,7 +18,6 @@ import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Point;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.util.SparseArray;
 import android.view.Gravity;
 import android.view.View;
