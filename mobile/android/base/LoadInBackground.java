3829ef360e3237b5d0f62d32f2560aa6c6480000
Martyn Haigh <martyn.haigh@gmail.com>
2014-12-10 16:09:33 +0000

    Add missing files

---
+++ mobile/android/base/LoadInBackground.java
@@ -0,0 +1,210 @@
+package org.mozilla.gecko;
+
+import org.mozilla.gecko.mozglue.ContextUtils;
+import org.mozilla.gecko.widget.ButtonToast;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by martyn on 03/12/14.
+ */
+public class LoadInBackground extends Activity {
+
+    private GeckoProfile mProfile;
+    private TextView mMessageView;
+    private Button mButton;
+    public static int LENGTH_SHORT = 3000;
+
+
+    private final Handler mHideHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+                        Intent forwardIntent = new Intent(getIntent());
+                forwardIntent.setClass(getApplicationContext(), LoadInBackgroundService.class);
+
+        startService(forwardIntent);
+        finish();
+
+//        LayoutInflater inflater = getLayoutInflater();
+
+//
+//        setCon(R.layout.button_toast,
+//                (ViewGroup) findViewById(R.id.toast));
+//        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+//
+//        mMessageView = (TextView) layout.findViewById(R.id.toast_message);
+//        mButton = (Button) layout.findViewById(R.id.toast_button);
+//        mButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent forwardIntent = new Intent(getIntent());
+//                forwardIntent.setClass(getApplicationContext(), BrowserApp.class);
+//                forwardIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                startActivity(forwardIntent);
+//
+//                hide();
+//            }
+//        });
+//
+//        mButton.setEnabled(true);
+//        mMessageView.setText("Open in background");
+//        mButton.setText("Open now");
+//
+//
+//        final int length = Toast.LENGTH_LONG;
+//        Toast toast = new Toast(getApplicationContext());
+//        toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+//        toast.setDuration(length);
+//        toast.setView(layout);
+//
+//
+//        toast.show();
+
+
+
+//        mButton.setCompoundDrawablesWithIntrinsicBounds(t.buttonDrawable, null, null, null);
+
+//        mHideHandler.postDelayed(mHideRunnable, length);
+//
+//        WindowManager.LayoutParams lp=dialog.getWindow().getAttributes();
+//        lp.x=100;
+//        lp.y=100;
+//        lp.width=100;
+//        lp.height=200;
+//        lp.gravity= Gravity.BOTTOM | Gravity.CENTER;
+//        lp.dimAmount=0;
+//        lp.flags= WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS | WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+//        //   dialog.getWindow().setAttributes(lp);
+//        dialog.setCanceledOnTouchOutside(true);
+//
+//
+//        mMessageView = (TextView) dialog.findViewById(R.id.toast_message);
+//        mButton = (Button) dialog.findViewById(R.id.toast_button);
+//        mButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent forwardIntent = new Intent(getIntent());
+//                forwardIntent.setClass(getApplicationContext(), BrowserApp.class);
+//                startActivity(forwardIntent);
+//
+//                dialog.hide();
+//            }
+//        });
+//        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+//            @Override
+//            public void onDismiss(DialogInterface dialog) {
+//                addUrlToList();
+//            }
+//        });
+//        dialog.show();
+
+//        getButtonToast().show(false, "Load in background",  ButtonToast.LENGTH_SHORT ,"Open now", getResources().getDrawable(R.drawable.switch_button_icon), new ButtonToast.ToastListener() {
+//            @Override
+//            public void onButtonClicked() {
+//                Intent forwardIntent = new Intent(getIntent());
+//                forwardIntent.setClass(getApplicationContext(), BrowserApp.class);
+//                startActivity(forwardIntent);
+//
+//                finish();
+//            }
+//
+//            @Override
+//            public void onToastHidden(ButtonToast.ReasonHidden reason) {
+//                if (reason == ButtonToast.ReasonHidden.TIMEOUT) {
+//                    addUrlToList();
+//
+//                }
+//
+//                finish();
+//            }
+//        });
+
+    }
+
+    private void hide() {
+        finish();
+    }
+
+    private void addUrlToList() {
+        final ContextUtils.SafeIntent intent = new ContextUtils.SafeIntent(getIntent());
+        final String action = intent.getAction();
+        final String args = intent.getStringExtra("args");
+        String data = intent.getDataString();
+        getProfile();
+
+        Log.d("MTEST", "Gecko in background: " + GeckoApplication.get().isApplicationInBackground());
+
+
+        if (mProfile == null) {
+            String profileName = null;
+            String profilePath = null;
+            if (args != null) {
+                if (args.contains("-P")) {
+                    Pattern p = Pattern.compile("(?:-P\\s*)(\\w*)(\\s*)");
+                    Matcher m = p.matcher(args);
+                    if (m.find()) {
+                        profileName = m.group(1);
+                    }
+                }
+
+                if (args.contains("-profile")) {
+                    Pattern p = Pattern.compile("(?:-profile\\s*)(\\S*)(\\s*)");
+                    Matcher m = p.matcher(args);
+                    if (m.find()) {
+                        profilePath = m.group(1);
+                    }
+                    if (profileName == null) {
+                        profileName = GeckoProfile.DEFAULT_PROFILE;
+                    }
+                    GeckoProfile.sIsUsingCustomProfile = true;
+                }
+
+                if (profileName != null || profilePath != null) {
+                    mProfile = GeckoProfile.get(this, profileName, profilePath);
+                }
+            }
+        }
+
+        try {
+            Log.d("MTEST", "Reading list contents: " + mProfile.readFile("temp_reading_list.json"));
+        } catch (IOException e) {
+            Log.d("MTEST", "Couldn't read : " + e.getMessage());
+
+            e.printStackTrace();
+        }
+        mProfile.appendToFile("temp_reading_list.json", data);
+    }
+
+    public synchronized GeckoProfile getProfile() {
+        // fall back to default profile if we didn't load a specific one
+        if (mProfile == null) {
+            mProfile = GeckoProfile.get(this);
+        }
+        return mProfile;
+    }
+    public ButtonToast getButtonToast() {
+        ViewStub toastStub = (ViewStub) findViewById(R.id.toast_stub);
+        return new ButtonToast(toastStub.inflate());
+    }
+    private final Runnable mHideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            addUrlToList();
+            hide();
+        }
+    };
+
+}
---
+++ mobile/android/base/LoadInBackgroundService.java
@@ -0,0 +1,191 @@
+package org.mozilla.gecko;
+
+import org.mozilla.gecko.mozglue.ContextUtils;
+
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LoadInBackgroundService extends Service {
+
+    private WindowManager windowManager;
+    private View layout;
+    private TextView mMessageView;
+    private Button mButton;
+    private GeckoProfile mProfile;
+    private final Handler mHideHandler = new Handler();
+    public static int LENGTH_SHORT = 3000;
+    private AtomicInteger mCount = new AtomicInteger(0);
+    private boolean mShown = false;
+    private WindowManager.LayoutParams mParams;
+    private Runnable mHideRunnable;
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // Not used
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        layout = layoutInflater.inflate(R.layout.button_toast, null);
+
+        mMessageView = (TextView) layout.findViewById(R.id.toast_message);
+        mButton = (Button) layout.findViewById(R.id.toast_button);
+
+
+        mButton.setEnabled(true);
+        mMessageView.setText("Open in background");
+        mButton.setText("Open now");
+
+        mParams = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.TYPE_PHONE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT);
+
+        mParams.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
+        mParams.x = 0;
+        mParams.y = 0;
+    }
+
+    @Override
+    public int onStartCommand(final Intent intent, int flags, int startId) {
+        Log.d("MTEST", "onStartCommand - count " + mCount);
+
+        mCount.incrementAndGet();
+        if (mHideRunnable != null) {
+            Log.d("MTEST", "runnable already running - run it immediately and cancel the timeout - count " + mCount);
+
+            mHideHandler.removeCallbacks(mHideRunnable);
+            mHideRunnable.run();
+        }
+
+        mHideRunnable = new Runnable() {
+            @Override
+            public void run() {
+                Log.d("MTEST", "hide runnable - count " + mCount);
+                mCount.decrementAndGet();
+
+                addUrlToList(intent);
+
+                if (mCount.get() == 0) {
+                    hide();
+                }
+                mHideRunnable = null;
+            }
+        };
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("MTEST", "button click - count " + mCount);
+
+                mCount.decrementAndGet();
+                mHideHandler.removeCallbacks(mHideRunnable);
+                mHideRunnable = null;
+
+                Intent forwardIntent = new Intent(intent);
+                forwardIntent.setClass(getApplicationContext(), BrowserApp.class);
+                forwardIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(forwardIntent);
+                hide();
+            }
+        });
+
+        windowManager.addView(layout, mParams);
+        mHideHandler.postDelayed(mHideRunnable, LENGTH_SHORT);
+        Log.d("MTEST", "SHOWING STUFF");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    private void hide() {
+        Log.d("MTEST", "HIDE!!!");
+        windowManager.removeView(layout);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //hide();
+    }
+
+
+    public synchronized GeckoProfile getProfile() {
+        // fall back to default profile if we didn't load a specific one
+        if (mProfile == null) {
+            mProfile = GeckoProfile.get(this);
+        }
+        return mProfile;
+    }
+
+    private void addUrlToList(Intent intentParam) {
+        final ContextUtils.SafeIntent intent = new ContextUtils.SafeIntent(intentParam);
+        final String action = intent.getAction();
+        final String args = intent.getStringExtra("args");
+        String data = intent.getDataString();
+        getProfile();
+
+        Log.d("MTEST", "Gecko in background: " + GeckoApplication.get().isApplicationInBackground());
+
+
+        if (mProfile == null) {
+            String profileName = null;
+            String profilePath = null;
+            if (args != null) {
+                if (args.contains("-P")) {
+                    Pattern p = Pattern.compile("(?:-P\\s*)(\\w*)(\\s*)");
+                    Matcher m = p.matcher(args);
+                    if (m.find()) {
+                        profileName = m.group(1);
+                    }
+                }
+
+                if (args.contains("-profile")) {
+                    Pattern p = Pattern.compile("(?:-profile\\s*)(\\S*)(\\s*)");
+                    Matcher m = p.matcher(args);
+                    if (m.find()) {
+                        profilePath = m.group(1);
+                    }
+                    if (profileName == null) {
+                        profileName = GeckoProfile.DEFAULT_PROFILE;
+                    }
+                    GeckoProfile.sIsUsingCustomProfile = true;
+                }
+
+                if (profileName != null || profilePath != null) {
+                    mProfile = GeckoProfile.get(this, profileName, profilePath);
+                }
+            }
+        }
+
+        try {
+            Log.d("MTEST", "Reading list contents: " + mProfile.readFile("temp_reading_list.json"));
+        } catch (IOException e) {
+            Log.d("MTEST", "Couldn't read : " + e.getMessage());
+
+            e.printStackTrace();
+        }
+        mProfile.appendToFile("temp_reading_list.json", data);
+    }
+
+}
+
