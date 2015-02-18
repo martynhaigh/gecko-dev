fbf14ae0799cbb0333b1fc885956879db668d3d6
Martyn Haigh <martyn.haigh@gmail.com>
2015-02-18 16:53:48 +0000

    remove import

--- mobile/android/base/tests/testSettingsMenuItems.java
+++ mobile/android/base/tests/testSettingsMenuItems.java
@@ -6,7 +6,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.ResourceBundle;
 
 import org.mozilla.gecko.Actions;
 import org.mozilla.gecko.AppConstants;
